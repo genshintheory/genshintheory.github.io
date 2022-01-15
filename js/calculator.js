5ad9880a7f5862e67a2f5762a6a478822b5bb2ec
genshintheory <ys.genshin.theory@gmail.com>
2022-01-15 18:30:38 -0500

    test

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,8 @@
 function add_number() {
    
-    var first_number = parseInt(document.getElementById("Text1").value);
-    var second_number = parseInt(document.getElementById("Text2").value);
-    var result = first_number + second_number;
+    var first_number = parseInt(document.getElementById("atk").value);
+    var second_number = parseInt(document.getElementById("dmgbonus").value);
+    var result = first_number * (1 + second_number/100);
 
     document.getElementById("txtresult").value = result;
 }
