35a6c438e4ebb0171f0259bde1e1fe3c4c544486
genshintheory <ys.genshin.theory@gmail.com>
2022-01-15 01:49:14 -0500

    test

---
+++ js/calculator.js
@@ -0,0 +1,8 @@
+function add_number() {
+   
+    var first_number = parseInt(document.getElementById("Text1").value);
+    var second_number = parseInt(document.getElementById("Text2").value);
+    var result = first_number + second_number;
+
+    document.getElementById("txtresult").value = result;
+}
