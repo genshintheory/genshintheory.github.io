bb3216d4627bf1535728d5ed5e94395af9120415
genshintheory <ys.genshin.theory@gmail.com>
2022-01-15 19:08:23 -0500

    expand

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,18 @@
-function add_number() {
+function dmg() {
    
-    var first_number = parseInt(document.getElementById("atk").value);
-    var second_number = parseInt(document.getElementById("dmgbonus").value);
-    var result = first_number * (1 + second_number/100);
+    var atk = parseFloat(document.getElementById("atk").value) || 2000;
+    var scal = parseFloat(document.getElementById("scal").value) || 100;
+    var dmgincrease = parseFloat(document.getElementById("dmgincrease").value) || 0;
+    var dmgbonus = parseFloat(document.getElementById("dmgbonus").value) || 0;
+    var cd = parseFloat(document.getElementById("cd").value) || 50;
 
-    document.getElementById("txtresult").value = result;
+    var outdmg = (atk * (scal/100) + dmgincrease) * (1 + dmgbonus/100);
+    var indmg = outdmg*.5*.9;
+    var indmg_crit = indmg * (1 + cd/100);
+    var mv = ((scal/100) + dmgincrease/2000) * (1 + dmgbonus/100) * 100
+
+    document.getElementById("txtresult").value = outdmg;
+    document.getElementById("indmg").value = indmg;
+    document.getElementById("indmg_crit").value = indmg_crit;
+    document.getElementById("mv").value = mv;
 }
